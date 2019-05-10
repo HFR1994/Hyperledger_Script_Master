6d1a6afef0b90e023f3f32e9856a7d6ffe3eb5a5
Hector Flores <frhectorin@gmail.com>
2019-05-10 15:07:12 -0500

    Add script for docker

---
+++ composer/CA-Replication/CA2/startCA2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -ev
+
+#Detect architecture
+ARCH=`uname -m`
+
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#Set IMAGETAG
+
+IMAGETAG="latest"
+
+IMAGETAG=$IMAGETAG ARCH=$ARCH docker-compose -f "${DIR}"/docker-compose-ca2.yml down
+IMAGETAG=$IMAGETAG ARCH=$ARCH docker-compose -f "${DIR}"/docker-compose-ca2.yml up -d
