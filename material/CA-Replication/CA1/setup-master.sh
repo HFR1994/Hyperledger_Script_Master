a44f13cd314c953366a65a50e6cef6f9d80655bc
Hector Flores <frhectorin@gmail.com>
2019-05-15 21:01:14 -0300

    Downgrade Fabric-CA file to 1.4.0

--- material/CA-Replication/CA1/setup-master.sh
+++ material/CA-Replication/CA1/setup-master.sh
@@ -3,7 +3,15 @@
 echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
+BEGIN
+   IF NOT EXISTS (
+      SELECT
+      FROM   pg_catalog.pg_user
+      WHERE  username = $PG_REP_USER) THEN
+
+      CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
+   END IF;
+END
 EOSQL
 cat >> ${PGDATA}/postgresql.conf <<EOF
 wal_level = hot_standby
