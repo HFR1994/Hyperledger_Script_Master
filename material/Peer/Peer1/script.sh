a0c7ba4f9cacc469b00b750799f95116de0cc5a3
Hector Flores <frhectorin@gmail.com>
2019-05-20 15:39:59 -0500

    Changed Peer Script

--- material/Peer/Peer1/script.sh
+++ material/Peer/Peer1/script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 export VERBOSE=false
 ARCH=`uname -m`
@@ -6,11 +6,11 @@ ARCH=`uname -m`
 # Exit on first error, print all commands.
 set -ev
 
-export CORE_PEER_MSPCONFIGPATH="/opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/users/Admin@04391007.hext.scd.org.br/msp"
+export CORE_PEER_MSPCONFIGPATH="/etc/hyperledger/config/msp/users/Admin@04391007.hext.scd.org.br/msp"
 
-peer channel create -o orderer-svc:7050 -c scd-deviceid -f /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/configtx/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
+peer channel create -o orderer-svc:7050 -c scd-deviceid -f /etc/hyperledger/config/configtx/channel.tx --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
 
-peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
-peer channel join -b scd-deviceid_config.block --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
+peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
+peer channel join -b scd-deviceid_config.block --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
 
-peer chaincode install -n deviceid -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Chaincode
+peer chaincode install -n deviceid -v 1.0 -l node -p /etc/hyperledger/config/peer/Peer/Chaincode
