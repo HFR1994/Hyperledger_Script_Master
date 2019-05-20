c2ea4e1c63ced221776a74d669dc2a9b187c7dff
Hector Flores <frhectorin@gmail.com>
2019-05-20 15:50:16 -0500

    Changed Script

--- material/Peer/Peer2/script.sh
+++ material/Peer/Peer2/script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export VERBOSE=false
 ARCH=`uname -m`
@@ -6,11 +6,9 @@ ARCH=`uname -m`
 # Exit on first error, print all commands.
 set -ev
 
-export CORE_PEER_MSPCONFIGPATH="/etc/hyperledger/config/msp/users/Admin@04391007.hext.scd.org.br/msp"
-
-peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
-peer channel join -b scd-deviceid_config.block --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
-peer channel update -o orderer-svc:7050 -c scd-deviceid -f /etc/hyperledger/config/configtx/04391007anchors.tx --tls --cafile /etc/hyperledger/config/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
+peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
+peer channel join -b scd-deviceid_config.block --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
+peer channel update -o orderer-svc:7050 -c scd-deviceid -f /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/configtx/04391007anchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
 
 peer chaincode install -n deviceid -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Chaincode
-peer chaincode instantiate --connTimeout 10 -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}' -P "OR('ISBP04391007.peer')"
+peer chaincode instantiate --connTimeout 10 -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}' -P "OR('ISBP04391007.peer')"
