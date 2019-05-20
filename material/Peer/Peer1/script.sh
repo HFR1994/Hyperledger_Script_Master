9be786ca64483336818b89094a91efa90ac6572e
Hector Carlos Flores Reynoso <frhectorin@gmail.com>
2019-05-19 22:24:34 -0500

    Restore to default

--- material/Peer/Peer1/script.sh
+++ material/Peer/Peer1/script.sh
@@ -10,6 +10,6 @@ peer channel create -o orderer-svc:7050 -c scd-deviceid -f /opt/gopath/src/githu
 
 peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
 peer channel join -b scd-deviceid_config.block --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
+peer channel update -o orderer-svc:7050 -c scd-deviceid -f /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/configtx/04391007anchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
 
 peer chaincode install -n deviceid -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Chaincode
-peer chaincode instantiate -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}'
