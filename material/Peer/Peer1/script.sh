6ee7c72226efc306f385a84b97bc774b16e4f91e
Hector Carlos Flores Reynoso <frhectorin@gmail.com>
2019-05-19 21:32:29 -0500

    Changed policy

--- material/Peer/Peer1/script.sh
+++ material/Peer/Peer1/script.sh
@@ -12,5 +12,4 @@ peer channel fetch config -o orderer-svc:7050 -c scd-deviceid --tls --cafile /op
 peer channel join -b scd-deviceid_config.block --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem
 
 peer chaincode install -n deviceid -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Chaincode
-peer chaincode instantiate -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}' -P "OR('ISBP04391007.peer')"
-
+peer chaincode instantiate -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}'
