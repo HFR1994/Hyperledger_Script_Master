23e7777b5adf03d32143e52905a551f1d7300eae
Hector Flores <frhectorin@gmail.com>
2019-05-20 18:45:47 -0500

    Change Policy

--- material/Peer/Peer2/script.sh
+++ material/Peer/Peer2/script.sh
@@ -11,4 +11,4 @@ peer channel join -b scd-deviceid_config.block --tls --cafile /opt/gopath/src/gi
 peer channel update -o orderer-svc:7050 -c scd-deviceid -f /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/configtx/04391007anchors.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer2/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem
 
 peer chaincode install -n deviceid -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Chaincode
-peer chaincode instantiate --connTimeout 10s -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}' -P "OR('ISBP04391007.member')"
+peer chaincode instantiate --connTimeout 10s -o orderer-svc:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/Peer/Peer1/msp/orderer/msp/tlscacerts/tlsca.orderer.hext.scd.org.br-cert.pem -C scd-deviceid -n deviceid -v 1.0 -c '{"Args":["initLedger"]}' -P "OR('ISPB04391007.member')"
